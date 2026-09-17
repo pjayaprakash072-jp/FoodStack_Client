df8c48036aa450e90669e84d817da66c7b2b7383
poojarijayaprakash4 <pjayaprakash072@gmail.com>
2026-09-17 12:04:25 +0530

    Added the Dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,44 @@
 
+import { ShoppingBag,ArrowRight,UtensilsCrossed } from 'lucide-react';
+import SearchBar from './../../components/Common/SearchBar';
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>
+      <section className="hero">
+        <div className="hero-content">
+          <span className="eyebrow">FoodStack Delivery</span>
+          <h1>Your favorite food,
+            <br />
+            <em>delivered</em>
+          </h1>
+          <p>
+            Discover Outlets, explore menus, and get delicious meals delivered to your door.
+          </p>
+          <div className="hero-search">
+            <SearchBar/>
+          </div>
+          <div className="hero-actions">
+            <ArrowRight/>
+            <ShoppingBag/>
+          </div>
+        </div>
+        <div className="hero-art">
+        <UtensilsCrossed size={100}/>
+        <span>🍕</span>
+        <span>🍔</span>
+        <span>🥗</span>
+        </div>
+      </section>
+      <section className="section">
+        <div className="section-heading">
+          <div>
+            <span className="eyebrow">Discover</span>
+            <h2>Popular Outlets</h2>
+          </div>
+            <div>View all outlets <ArrowRight/></div>
+        </div>
+      </section>
+    </div>
   )
 }
 
