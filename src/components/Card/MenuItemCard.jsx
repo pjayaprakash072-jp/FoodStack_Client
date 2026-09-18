08b3fca1c7b9d141d598edab8f84a8a923625189
poojarijayaprakash4 <pjayaprakash072@gmail.com>
2026-09-18 13:59:23 +0530

    Added the menuItem Card

---
+++ src/components/Card/MenuItemCard.jsx
@@ -0,0 +1,27 @@
+import { Plus } from "lucide-react"
+
+const MenuItemCard = ({menuItem}) => {
+    const image = menuItem.image?.url
+  return (
+    <article className="menuItem-card">
+        <div className="menuItem-info">
+            <h3>{menuItem.name || "Menu Item"}</h3>
+            <strong>{menuItem.price}</strong>
+            <p>{menuItem.description || "Freshly prepared and served with care."}</p>
+            <button className="button">
+                <Plus size={16}/>
+                Add
+            </button>
+        </div>
+        {
+            image?(
+                <img src={image} alt={menuItem.name || "Food"}/>
+            ):(
+                <div className="food-placeholder">🍲</div>
+            )
+        }
+    </article>
+  )
+}
+
+export default MenuItemCard
